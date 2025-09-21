055f50c102786dfdeefba71ad7b719461c118f8c
Camila Cabrera <papu_serrana@outlook.com>
2025-09-21 00:33:38 -0500

    si

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,29 @@
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "project" { type = string, default = "examapp" }
-variable "db_username" { type = string, default = "labsafe" }
-variable "db_password" { type = string, default = "labsafe" }
-variable "db_name" { type = string, default = "examsdb" }
-variable "instance_type" { type = string, default = "t3.micro" }
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "project" {
+  type    = string
+  default = "examapp"
+}
+
+variable "db_username" {
+  type    = string
+  default = "labsafe"
+}
+
+variable "db_password" {
+  type    = string
+  default = "labsafe"
+}
+
+variable "db_name" {
+  type    = string
+  default = "examsdb"
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t3.micro"
+}
